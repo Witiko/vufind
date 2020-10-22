1e6c700332efc7a17cc05d3a532ff775516bae5f
Michal Růžička <ruzicka@ics.muni.cz>
2020-10-22 17:52:33 +0200

    Do not ask Aleph API for eloan availability
    It is not necessary now as:
    
    1. IP address of the client (needed only for recognizing particular
       clients for debugging) is passed as second argument of eloan_tag
       function.
    
       See commit e91a323728c105ef9254018913073fc8f01bdadc
    
    2. eloan availability is not needed to check in Aleph as eprez.js is
       included in the page only if the record has eloan.
    
       See commit 3a50c150496268858aaed8fb9ae17e4201c3e135

--- themes/muni/js/eprez.js
+++ themes/muni/js/eprez.js
@@ -1,4 +1,4 @@
-function eprez_tag(sysno) {
+function eprez_tag(sysno, client_ip) {
 
   if (sysno.substring(0, 5) != 'MUB01') {
     return;
@@ -6,78 +6,40 @@ function eprez_tag(sysno) {
 
   sysno = sysno.replace(/^MUB0[1-3]/, "");
 
-  var url = "https://katalog.muni.cz/terminal-ips.txt";
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var terminal_ips = this.responseText.split('\n').slice(0, -1);
-      // console.log(terminal_ips);
-      query_aleph(terminal_ips);
-    }
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send();
-
-  function query_aleph(terminal_ips) {
-    var eprez = document.getElementById("eprez");
-    var time = new Date().getTime();
-    var url = "https://kic.ics.muni.cz/eprezencka/alephApi/toAleph.pl?sysno=" + sysno + "&jsoncallback=eprez_tag_callback&_=" + time;
-    var xhttp = new XMLHttpRequest();
-
-    function in_array(element, array) {
-      var in_array = false;
-      for (var i = 0; i < array.length; i++) {
-        if (array[i] === element) {
-          in_array = true;
-          break;
-        }
-      }
-      return in_array;
-    }
-
-    function eprez_tag_callback() {
-      data = {};
-      for (var i = 0; i < arguments.length; ++i) {
-        for (var j in arguments[i]) {
-          data[j] = arguments[i][j];
-        }
-      }
-
-      test_ips = ['147.251.56.248',
-                  '147.251.2.60',
-                  '147.251.2.97',
-                  '147.251.56.209',
-                  '147.251.68.59',
-                  '147.251.17.76',
-                  '188.95.60.153'];
-
-      if("show" in data) {
-        var a = document.createElement("a");
-        if (in_array(data["ip"], terminal_ips) || in_array(data["ip"], test_ips)) {
-          a.href = "https://kic.ics.muni.cz/e-prezencka.cgi?sysno=" + sysno;
-          //a.href = "https://kic.ics.muni.cz/eprezencka/pdf/" + sysno + ".pdf";
-        //} else {
-        //  a.href = "https://it.muni.cz/knihovny-samostudium/e-prezencka";
-        }
-        if (in_array(data["ip"], terminal_ips) || in_array(data["ip"], test_ips)) {
-            a.appendChild(document.createTextNode(VuFind.translate("muni::eloan_terminal")));
-            a.style.color = "red";
-            a.style.fontWeight = "900";
-        //} else {
-        //    a.appendChild(document.createTextNode(VuFind.translate("muni::eloan")));
-        }
-        var h3 = document.createElement("h3");
-        h3.appendChild(a);
-        eprez.appendChild(h3);
+  function in_array(element, array) {
+    var in_array = false;
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] === element) {
+        in_array = true;
+        break;
       }
     }
-
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        eval(this.responseText);
-      }
-    };
-    xhttp.open("GET", url, true);
-    xhttp.send();
+    return in_array;
   }
+
+  test_ips = ['147.251.56.248',
+              '147.251.2.60',
+              '147.251.2.97',
+              '147.251.56.209',
+              '147.251.68.59',
+              '147.251.17.76',
+              '188.95.60.153',
+              '95.82.147.69',
+              '95.82.184.88',
+              '81.19.8.196'];
+
+  var eprez = document.getElementById("eprez");
+
+  var a = document.createElement("a");
+  if (in_array(client_ip, test_ips)) {
+    a.href = "https://kic.ics.muni.cz/e-prezencka.cgi?sysno=" + sysno;
+    a.appendChild(document.createTextNode(VuFind.translate("muni::eloan_terminal")));
+    a.style.color = "red";
+    a.style.fontWeight = "900";
+  }
+  var h3 = document.createElement("h3");
+  h3.appendChild(a);
+
+  eprez.appendChild(h3);
+
 }
